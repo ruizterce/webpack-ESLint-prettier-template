2dce18001618592e34d427383320a031eb4f1603
ruiz.terce <ruiz.terce@gmail.com>
2024-05-30 17:23:46 +0200

    Configure static assets handling in jest

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,10 @@ module.exports = {
     '^.+\\.js$': 'babel-jest'
   },
   testEnvironment: 'node',
-  moduleDirectories: ['node_modules', 'src']
+  moduleDirectories: ['node_modules', 'src'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js'
+  }
 };
